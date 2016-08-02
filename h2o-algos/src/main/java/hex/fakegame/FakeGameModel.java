dceb0ffb8e69caf0f96bbc28346a53cb788ddb70
Bc. Tomáš Frýda <frydatom@fit.cvut.cz>
2016-08-02 15:52:49 +0200

    newer version of game libs

--- h2o-algos/src/main/java/hex/fakegame/FakeGameModel.java
+++ h2o-algos/src/main/java/hex/fakegame/FakeGameModel.java
@@ -7,64 +7,93 @@ import water.Key;
 
 import java.util.LinkedList;
 
-public class FakeGameModel extends Model<FakeGameModel,FakeGameModel.FakeGameParameters,FakeGameModel.FakeGameOutput> {
+public class FakeGameModel extends Model<FakeGameModel, FakeGameModel.FakeGameParameters, FakeGameModel.FakeGameOutput> {
 
-  public static class FakeGameParameters extends Model.Parameters {
-    @Override public String algoName() { return "FakeGame"; }
-    @Override public String fullName() { return "FakeGame"; }
-    @Override public String javaName() { return FakeGameModel.class.getName(); }
-    @Override public long progressUnits() { return 1; }
-    public String _classifier_config;
-  }
+    public static class FakeGameParameters extends Model.Parameters {
+        @Override
+        public String algoName() {
+            return "FakeGame";
+        }
 
-  public static class FakeGameOutput extends Model.Output {
+        @Override
+        public String fullName() {
+            return "FakeGame";
+        }
 
-    public LinkedList<Classifier> _cls;
+        @Override
+        public String javaName() {
+            return FakeGameModel.class.getName();
+        }
 
-    public FakeGameOutput( FakeGame b ) { super(b); }
-    @Override public ModelCategory getModelCategory() {
-      if (isClassifier()){
-        return (nclasses()>2)? ModelCategory.Multinomial : ModelCategory.Binomial;
-      }
-      return ModelCategory.Regression;
+        @Override
+        public long progressUnits() {
+            return 1;
+        }
+
+        public String _classifier_config;
     }
 
-    @Override public boolean isSupervised() { return true; }
-  }
+    public static class FakeGameOutput extends Model.Output {
 
-  FakeGameModel( Key selfKey, FakeGameParameters parms, FakeGameOutput output) { super(selfKey,parms,output); }
+        public LinkedList<Classifier> _cls;
 
-  @Override
-  public ModelMetrics.MetricBuilder makeMetricBuilder(String[] domain) {
-    switch(_output.getModelCategory()) {
-      case Binomial:    return new ModelMetricsBinomial.MetricBuilderBinomial(domain);
-      case Multinomial: return new ModelMetricsMultinomial.MetricBuilderMultinomial(domain.length,domain);
-      default: throw H2O.unimpl();
-    }
-  }
+        public FakeGameOutput(FakeGame b) {
+            super(b);
+        }
 
-  @Override protected double[] score0(double data[/*ncols*/], double preds[/*nclasses+1*/]) {
-    for (int i = 0; i < _output.nclasses()+1; i++) {
-      preds[i] = 0.0;
-    }
+        @Override
+        public ModelCategory getModelCategory() {
+            if (isClassifier()) {
+                return (nclasses() > 2) ? ModelCategory.Multinomial : ModelCategory.Binomial;
+            }
+            return ModelCategory.Regression;
+        }
 
-    for (Classifier c :_output._cls) {
-      preds[1+c.getOutput(data)] += 1.0 / _output._cls.size();
-    }
-
-    double max = -1.0;
-    for (int i = 1; i < _output.nclasses()+1; i++) {
-      if (preds[i] > max)
-        max = preds[i];
+        @Override
+        public boolean isSupervised() {
+            return true;
+        }
     }
 
-    for (int i = 1; i < _output.nclasses()+1; i++) {
-      if (preds[i] == max) {
-        preds[0] = i;
-        break;
-      }
+    FakeGameModel(Key selfKey, FakeGameParameters parms, FakeGameOutput output) {
+        super(selfKey, parms, output);
     }
-    return preds;
-  }
+
+    @Override
+    public ModelMetrics.MetricBuilder makeMetricBuilder(String[] domain) {
+        switch (_output.getModelCategory()) {
+            case Binomial:
+                return new ModelMetricsBinomial.MetricBuilderBinomial(domain);
+            case Multinomial:
+                return new ModelMetricsMultinomial.MetricBuilderMultinomial(domain.length, domain);
+            default:
+                throw H2O.unimpl();
+        }
+    }
+
+    @Override
+    protected double[] score0(double data[/*ncols*/], double preds[/*nclasses+1*/]) {
+        for (int i = 0; i < _output.nclasses() + 1; i++) {
+            preds[i] = 0.0;
+        }
+
+        for (Classifier c : _output._cls) {
+            preds[1 + c.getOutput(data)] += 1.0 / _output._cls.size();
+        }
+
+        double max = -1.0;
+        for (int i = 1; i < _output.nclasses() + 1; i++) {
+            if (preds[i] > max)
+                max = preds[i];
+        }
+
+        for (int i = 1; i < _output.nclasses() + 1; i++) {
+            if (preds[i] == max) {
+                preds[0] = i;
+                break;
+            }
+        }
+        return preds;
+    }
 
 }
--- h2o-py/h2o/estimators/__init__.py
+++ h2o-py/h2o/estimators/__init__.py
@@ -5,21 +5,21 @@
 # Copyright 2016 H2O.ai;  Apache License Version 2.0 (see LICENSE for details)
 #
 from .deeplearning import H2OAutoEncoderEstimator
-from .gbm import H2OGradientBoostingEstimator
-from .naive_bayes import H2ONaiveBayesEstimator
+from .glrm import H2OGeneralizedLowRankEstimator
+from .glm import H2OGeneralizedLinearEstimator
 from .aggregator import H2OAggregatorEstimator
+from .svd import H2OSingularValueDecompositionEstimator
+from .gbm import H2OGradientBoostingEstimator
+from .kmeans import H2OKMeansEstimator
+from .naive_bayes import H2ONaiveBayesEstimator
+from .deeplearning import H2ODeepLearningEstimator
 from .fakegame import H2OFakegameEstimator
 from .pca import H2OPrincipalComponentAnalysisEstimator
 from .random_forest import H2ORandomForestEstimator
-from .svd import H2OSingularValueDecompositionEstimator
-from .kmeans import H2OKMeansEstimator
-from .glrm import H2OGeneralizedLowRankEstimator
-from .glm import H2OGeneralizedLinearEstimator
-from .deeplearning import H2ODeepLearningEstimator
 
 __all__ = [
-    "H2OAutoEncoderEstimator", "H2OGradientBoostingEstimator", "H2ONaiveBayesEstimator", "H2OAggregatorEstimator",
-    "H2OFakegameEstimator", "H2OPrincipalComponentAnalysisEstimator", "H2ORandomForestEstimator",
-    "H2OSingularValueDecompositionEstimator", "H2OKMeansEstimator", "H2OGeneralizedLowRankEstimator",
-    "H2OGeneralizedLinearEstimator", "H2ODeepLearningEstimator"
+    "H2OAutoEncoderEstimator", "H2OGeneralizedLowRankEstimator", "H2OGeneralizedLinearEstimator",
+    "H2OAggregatorEstimator", "H2OSingularValueDecompositionEstimator", "H2OGradientBoostingEstimator",
+    "H2OKMeansEstimator", "H2ONaiveBayesEstimator", "H2ODeepLearningEstimator", "H2OFakegameEstimator",
+    "H2OPrincipalComponentAnalysisEstimator", "H2ORandomForestEstimator"
 ]
